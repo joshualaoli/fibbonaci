0839768a737220e76a0c80a1dad5fc16d4bd17a5
Joshua Laoli <joe.pixarts@gmail.com>
2023-05-04 11:39:22 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+function fibbonaciNumber(n) {
+    var output = [];
+    if (n === 1) {
+        output = [0];
+    }
+    else if (n === 2){
+        output = [0, 1];
+    }
+    else {
+        output = [0, 1]; // length =2
+
+        for(var i=2; i < n ; i++)
+        output.push(output[output.length - 2 ] + output[output.length - 1]);//0,1,1
+    }
+
+    return output;
+} 
+
+var output = fibbonaciNumber(10);
+console.log(output);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Goal
+/*
+
+function fibonacciGenerator (n) {
+//Do NOT change any of the code above 👆
+    
+    //Write your code here:
+    
+    var output = [];
+  if (n === 1) {
+    output = [0];
+  }
+  else if (n === 2) {
+    output = [0, 1]
+  }
+  else {
+    output = [0, 1];
+    for (var i = 2; i < n; i++)
+      output.push(output[output.length - 2] + output[output.length - 1]); // 0, 1, 1
+  }
+
+  return output
+    
+    //Return an array of fibonacci numbers starting from 0.
+    
+//Do NOT change any of the code below 👇
+}
+
+
+*/
+
+
+
+
+
